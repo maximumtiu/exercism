dbbfebf48326e7fab26865a9674530be5ea5e9a9
Megan Tiu <megantiu@me.com>
2016-07-01 22:21:06 -0400

    Refactoring hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,8 @@
 class Hamming
   VERSION = 2
 
-  def self.compute(str_one, str_two)
-    raise ArgumentError if str_one.length != str_two.length
-
-    count = 0
-    str_one.chars.each_with_index do |char, index|
-      if char != str_two[index]
-        count += 1
-      end
-    end
-    count
+  def self.compute(str_a, str_b)
+    raise ArgumentError if str_a.length != str_b.length
+    str_a.chars.each_index.count { |ind| str_a[ind] != str_b[ind] }
   end
 end
